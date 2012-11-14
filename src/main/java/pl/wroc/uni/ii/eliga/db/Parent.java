56e5f66313379597ac9c133d8dad5daffcc08c36
Piotr Olma <perdo.olma@gmail.com>
2012-11-14 09:42:48 +0100

    Add model fields.

--- src/main/java/pl/wroc/uni/ii/eliga/db/Parent.java
+++ src/main/java/pl/wroc/uni/ii/eliga/db/Parent.java
@@ -1,5 +1,20 @@
 package pl.wroc.uni.ii.eliga.db;
 
 public class Parent {
-
+	private final int id;
+	private final String name;
+	private final String surname;
+	private final String email;
+	private final String phone;
+	private final int studentId;
+	
+	public Parent(int id, String name, String surname, String email,
+			String phone, int studentId) {
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.email = email;
+		this.phone = phone;
+		this.studentId = studentId;
+	}
 }
--- src/main/java/pl/wroc/uni/ii/eliga/db/Teacher.java
+++ src/main/java/pl/wroc/uni/ii/eliga/db/Teacher.java
@@ -1,5 +1,12 @@
 package pl.wroc.uni.ii.eliga.db;
 
 public class Teacher {
-
+	private final int id;
+	private final String name;
+	private final String surname;
+	public Teacher(int id, String name, String surname) {
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+	}
 }
