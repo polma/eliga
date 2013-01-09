f4216e77016df74141204b67b55652335ba7f81a
dawidVIFO <dawid.ryznar@yahoo.com>
2013-01-09 09:51:58 +0100

    add proof of concept for gui test

---
+++ src/acceptance/java/pl/wroc/uni/ii/eliga/test/gui/GuiTest.java
@@ -0,0 +1,29 @@
+package pl.wroc.uni.ii.eliga.test.gui;
+
+import org.fest.swing.testng.testcase.FestSwingTestngTestCase;
+
+public class GuiTest extends FestSwingTestngTestCase {
+	
+	// author : http://docs.codehaus.org/display/FEST/Getting+Started. Please do not run.
+	
+//	 private FrameFixture window;
+//	 
+	  protected void onSetUp() {
+//	    MyFrame frame = GuiActionRunner.execute(new GuiQuery<MyFrame>() {
+//	        protected MyFrame executeInEDT() {
+//	          return new MyFrame();  
+//	        }
+//	    });
+//	    // IMPORTANT: note the call to 'robot()'
+//	    // we must use the Robot from FestSwingTestngTestCase
+//	    window = new FrameFixture(robot(), frame);
+//	    window.show(); // shows the frame to test
+	  }
+//	 
+//	  @Test public void shouldCopyTextInLabelWhenClickingButton() {
+//	    window.textBox("textToCopy").enterText("Some random text");
+//	    window.button("copyButton").click();
+//	    window.label("copiedText").requireText("Some random text");
+//	  }
+
+}
