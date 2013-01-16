634a631797d52159dc2f1dbe961f3ff6edce7aa5
dawidVIFO <dawid.ryznar@yahoo.com>
2013-01-16 09:54:01 +0100

    add runner to test

--- src/acceptance/java/pl/wroc/uni/ii/eliga/test/ui/GuiTest.java
+++ src/acceptance/java/pl/wroc/uni/ii/eliga/test/ui/GuiTest.java
@@ -4,22 +4,31 @@ import org.fest.swing.edt.FailOnThreadViolationRepaintManager;
 import org.fest.swing.edt.GuiActionRunner;
 import org.fest.swing.edt.GuiQuery;
 import org.fest.swing.fixture.FrameFixture;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import pl.wroc.uni.ii.eliga.common.EligaInjector;
 import pl.wroc.uni.ii.eliga.db.DatabaseServiceTest;
+import pl.wroc.uni.ii.eliga.test.util.GuiceJUnitRunner;
 import pl.wroc.uni.ii.eliga.test.util.TestObjects;
 import pl.wroc.uni.ii.eliga.ui.NoticeAdder;
 
+import com.google.inject.Inject;
+@RunWith(GuiceJUnitRunner.class)
 public class GuiTest {
 	
 	private FrameFixture window;
-
+	@Inject
+	DatabaseServiceTest service;
+	
+	@Before
 	public void setUpOnce() {
 		FailOnThreadViolationRepaintManager.install();
-		
 	}
-
+	
+	@Before
 	public void setUp() {
 		NoticeAdder frame = GuiActionRunner.execute(new GuiQuery<NoticeAdder>() {
 			protected NoticeAdder executeInEDT() {
@@ -28,27 +37,23 @@ public class GuiTest {
 		});
 		window = new FrameFixture(frame);
 		window.show(); // shows the frame to test
+		
+		service.cleanUp();
+		service.insertAll();		
 	}
 
+	@After
 	public void tearDown() {
 		window.cleanUp();
 	}
 
 	@Test
 	public void shouldCopyTextInLabelWhenClickingButton() throws InterruptedException {
-		DatabaseServiceTest service = EligaInjector.getInstance(DatabaseServiceTest.class);
-		service.cleanUp();
-		service.insertAll();
-		setUpOnce();
-		setUp();
-		
 		window.checkBox("jCheckBox1").check();
 		window.textBox("jTextArea1").setText("Some notice");
 		window.textBox("jTextField1").enterText(TestObjects.STUDENT.getPesel());
 		window.button("jButton1").click();
 		window.comboBox("jComboBox1").selectItem(0);
-		
-		tearDown();
 	}
 
 }
