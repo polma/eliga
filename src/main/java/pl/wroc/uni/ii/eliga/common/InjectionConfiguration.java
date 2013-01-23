2188ca348a17b7327d2660331159e8fbb62cd452
Przemek Pastuszka <pastuszka.przemyslaw@gmail.com>
2013-01-23 08:53:46 +0100

    quick fix for nested transactions

--- src/main/java/pl/wroc/uni/ii/eliga/common/InjectionConfiguration.java
+++ src/main/java/pl/wroc/uni/ii/eliga/common/InjectionConfiguration.java
@@ -1,8 +1,14 @@
 package pl.wroc.uni.ii.eliga.common;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
+import static com.google.inject.matcher.Matchers.annotatedWith;
+import static com.google.inject.matcher.Matchers.any;
+import static org.hibernate.cfg.AvailableSettings.DIALECT;
+import static org.hibernate.cfg.AvailableSettings.DRIVER;
+import static org.hibernate.cfg.AvailableSettings.PASS;
+import static org.hibernate.cfg.AvailableSettings.URL;
+import static org.hibernate.cfg.AvailableSettings.USER;
+import static pl.wroc.uni.ii.eliga.common.EligaInjector.getInstance;
+
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.hibernate.Session;
@@ -11,6 +17,11 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.dialect.PostgreSQL82Dialect;
 import org.postgresql.Driver;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+
 import pl.wroc.uni.ii.eliga.db.DatabaseService;
 import pl.wroc.uni.ii.eliga.db.hibernate.HibernateDatabaseService;
 import pl.wroc.uni.ii.eliga.db.model.Course;
@@ -21,15 +32,6 @@ import pl.wroc.uni.ii.eliga.db.model.Student;
 import pl.wroc.uni.ii.eliga.db.model.Teacher;
 import pl.wroc.uni.ii.eliga.mail.GmailSender;
 import pl.wroc.uni.ii.eliga.mail.MailSender;
-
-import static com.google.inject.matcher.Matchers.annotatedWith;
-import static com.google.inject.matcher.Matchers.any;
-import static org.hibernate.cfg.AvailableSettings.DIALECT;
-import static org.hibernate.cfg.AvailableSettings.DRIVER;
-import static org.hibernate.cfg.AvailableSettings.PASS;
-import static org.hibernate.cfg.AvailableSettings.URL;
-import static org.hibernate.cfg.AvailableSettings.USER;
-import static pl.wroc.uni.ii.eliga.common.EligaInjector.getInstance;
 
 public class InjectionConfiguration extends AbstractModule {
   private static ThreadLocal<Session> threadLocalSession = new ThreadLocal<Session>();
@@ -83,19 +85,37 @@ public class InjectionConfiguration extends AbstractModule {
   private static class TransactionalInterceptor implements MethodInterceptor {
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
-      Session session = getInstance(SessionFactory.class).openSession();
-      threadLocalSession.set(session);
-      Transaction transaction = session.beginTransaction();
-      try {
-        Object result = invocation.proceed();
-        transaction.commit();
-        return result;
-      } catch (Throwable t) {
-        transaction.rollback();
-        throw t;
-      } finally {
-        session.close();
+      if (threadLocalSession.get() != null
+          && threadLocalSession.get().isOpen()) {
+        Session session = threadLocalSession.get();
+        Transaction transaction = session.getTransaction();
+
+        try {
+          Object result = invocation.proceed();
+          //transaction.commit();
+          return result;
+        } catch (Throwable t) {
+          //transaction.rollback();
+          throw t;
+        }
       }
+      else {
+        Session session = getInstance(SessionFactory.class).openSession();
+        threadLocalSession.set(session);
+        Transaction transaction = session.beginTransaction();
+
+        try {
+          Object result = invocation.proceed();
+          transaction.commit();
+          return result;
+        } catch (Throwable t) {
+          transaction.rollback();
+          throw t;
+        } finally {
+          session.close();
+        }
+      }
+
     }
   }
 }
